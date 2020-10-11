302c8e50094078308d8c7a59b9b501aaaf720946
Alexey Delendik <delendikalexey@gmail.com>
2020-10-11 19:19:27 +0300

    12 проектная работа версия 2

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require('path');
-// const userRoutes = require('./routes/users.js');
-// const cardRoute = require('./routes/cards.js');
-// const errorRoute = require('./routes/error.js');
 const routes = require('./routes/index.js');
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/', userRoutes);
-// app.use('/', cardRoute);
-// app.use('/', errorRoute);
 app.use(routes);
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
-const express = require('express');
-
-const app = express();
-const routerrr = require('express').Router();
+const router = require('express').Router();
 
 const userRoutes = require('./users.js');
 const cardRoute = require('./cards.js');
 const errorRoute = require('./error.js');
 
-app.use('/', userRoutes);
-app.use('/', cardRoute);
-app.use('/', errorRoute);
+router.use('/', userRoutes);
+router.use('/', cardRoute);
+router.use('/', errorRoute);
 
-module.exports = routerrr;
+module.exports = router;
